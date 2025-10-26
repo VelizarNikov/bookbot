c66eb6d860e459384d6c6bd625fb6ade0378b179
VelizarNikov <nikovvelizar@gmail.com>
2025-10-26 08:01:15 +0000

    005: added stats.py moved word_count form main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ def get_book_text(path_to_file):
 	with open(path_to_file) as f:
 		return f.read()
 	
-def word_count(text):
-	word_list = text.split()
-	return len(word_list)
+from stats import word_count
 
 def main():
 	path = "books/frankenstein.txt"
---
+++ stats.py
@@ -0,0 +1,3 @@
+def word_count(text):
+	word_list = text.split()
+	return len(word_list)

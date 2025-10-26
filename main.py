6a96a2a5860dcaf1ea0d9a345e7eab20be4facac
VelizarNikov <nikovvelizar@gmail.com>
2025-10-26 07:32:33 +0000

    004: main.py does word count

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 def get_book_text(path_to_file):
-	
 	with open(path_to_file) as f:
-		file_contents = f.read()
-	return file_contents
+		return f.read()
+	
+def word_count(text):
+	word_list = text.split()
+	return len(word_list)
 
 def main():
-	print(get_book_text("books/frankenstein.txt"))
-
+	path = "books/frankenstein.txt"
+	text = get_book_text(path)
+	num_words = word_count(text)
+	print(f"Found {num_words} total words")
 
 main()
